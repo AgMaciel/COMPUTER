b060d3fde040caa98354a713fba4a8cf8a377f86
AgMaciel <120415264+AgMaciel@users.noreply.github.com>
2023-01-16 21:30:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function escreveTexto() {
+    let x = document.querySelector("#texto");
+    x.innerText = 'SEJA BEM VINDO!';
+
+}
+
+function voltaTexto() {
+    let x = document.querySelector('#texto');
+    x.innerText = ' A²J | História e Evolução dos Computadores';
+}
